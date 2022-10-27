ea2c3e07f47d00e2446b50c6874ca27283521dfd
AdesounDeji <HP@Ghostman.(none)>
2022-10-27 20:02:17 +0100

    solution

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import { errorResponse, handleError, } from "./utils/responses";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const details = {
+    slackUsername: "Adeosun Deji",
+    backend: true,
+    Age: 26,
+    Bio: "I'm a Backend dev(Nodejs). When i'm not coding, I love playing video games and playing football. My tech journey has been amazing so far vand i just want to keep doing my best so i can be a world class develpoer."
+}
+
+app.get("/getUser", (req, res) => {
+    try {
+        const details = {
+            slackUsername: "Adeosun Deji",
+            backend: true,
+            Age: 26,
+            Bio: "I'm a Backend dev(Nodejs). When i'm not coding, I love playing video games and playing football. My tech journey has been amazing so far vand i just want to keep doing my best so i can be a world class develpoer."
+        };
+        return res.json(details);
+    } catch (error) {
+        handleError(error, req);
+        return errorResponse(res, 500, "server error..");
+    }
+})
+
+app.listen(2000, () => {
+    console.log("listening on port 2000");
+})
