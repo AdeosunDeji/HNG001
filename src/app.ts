00abb3dd8f2d7b0e62109597e1b106b76d4cf7af
AdesounDeji <HP@Ghostman.(none)>
2022-10-29 22:46:52 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const details = {
-    slackUsername: "Adeosun Deji",
-    backend: true,
-    Age: 26,
-    Bio: "I'm a Backend dev(Nodejs). When i'm not coding, I love playing video games and playing football. My tech journey has been amazing so far vand i just want to keep doing my best so i can be a world class develpoer."
-}
-
 app.get("/getUser", (req, res) => {
     try {
         const details = {
