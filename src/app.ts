b28102ea9fdebb8ae976e5b605d812636e7b69db
AdesounDeji <HP@Ghostman.(none)>
2022-10-30 06:28:31 +0100

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 import express from "express";
 import cors from "cors";
 import { errorResponse, handleError, } from "./utils/responses";
+import { IUser } from "./utils/interface";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/getUser", (req, res) => {
+app.get("/", (req, res) => {
     try {
-        const details = {
+        const details: IUser = {
             slackUsername: "Adeosun Deji",
             backend: true,
-            Age: 26,
-            Bio: "I'm a Backend dev(Nodejs). When i'm not coding, I love playing video games and playing football. My tech journey has been amazing so far vand i just want to keep doing my best so i can be a world class develpoer."
+            age: 26,
+            bio: "I'm a Backend dev(Nodejs). When i'm not coding, I love playing video games and playing football. My tech journey has been amazing so far vand i just want to keep doing my best so i can be a world class develpoer."
         };
         return res.json(details);
     } catch (error) {
---
+++ src/utils/interface.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+    slackUsername?: string;
+    backend: boolean;
+    age?: number;
+    bio?: string;
+}
